3343e70f3918a57477d57fb84c37d4f32d3076e0
Doro256 <dorothynantumbwe44@gmail.com>
2022-07-06 20:15:51 +0000

    key with the biggest integer value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary == {}:
-        return None
-    else:
-        return (max(a_dictionary.keys()))
+    return (max(a_dictionary.keys()))
