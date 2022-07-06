b63294303fe7f3eebc437a01a641f736303b5c59
Doro256 <dorothynantumbwe44@gmail.com>
2022-07-06 18:12:40 +0000

    Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python
 def search_replace(my_list, search, replace):
-    return (list(map(lambda x: x.replace if x is search else x, my_list)))
+    return (list(map(lambda x: replace if x is search else x, my_list)))
