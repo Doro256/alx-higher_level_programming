13501cf8b24bf1295b5e7d985f8d3a8ddefc0194
Doro256 <dorothynantumbwe44@gmail.com>
2022-07-18 21:40:04 +0000

    divides element by element 2 lists

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -22,5 +22,5 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
         finally:
-            new_list.append(div)
+            new_list.append(result)
     return (new_list)
