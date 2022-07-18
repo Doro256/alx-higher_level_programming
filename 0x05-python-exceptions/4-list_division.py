6e46e0426a574e55e0c716012f2d4657e2b5560f
Doro256 <dorothynantumbwe44@gmail.com>
2022-07-18 21:33:42 +0000

    divides element by element 2 lists

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -15,13 +15,10 @@
             div = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong type")
-            div = 0
         except ZeroDivisionError:
             print("division by 0")
-            div = 0
         except IndexError:
             print("out of range")
-            div = 0
         finally:
             new_list.append(div)
     return (new_list)
