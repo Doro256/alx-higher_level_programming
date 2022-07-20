be740d7a5fbce1d7aa5ea2be2364eb1976617b46
Doro256 <dorothynantumbwe44@gmail.com>
2022-07-20 13:19:03 +0000

    4Square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,44 +1,52 @@
 #!/usr/bin/python3
-
-"""Define a class Square."""
+""" module contains: class Square """
 
 
 class Square:
-    """A square class"""
-
-
-
-def __init__(self, size=0):
-    """Initialize class
-    
-    Args:
-        size (int): Size of the square
     """
-    self.size = size
+        Square: defines a square
+        Attributes:
+            size (int): size of square
+        Method:
+            __init__: Initializes size for all instances
+    """
 
-    if size != int(size):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = size
+    def __init__(self, size=0):
+        """ Initialization of attributes for instances
+            Args:
+                size (int): size of the square.
+        """
+        if (isinstance(size, int)):
+            self.__size = size
+            if size < 0:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
 
+    @property
+    def size(self):
+        """ getter function to for private attribute size
+            Return:
+                size of the square
+        """
+        return self.__size
 
-def area(self):
-    """Returns the current square area"""
-    return (self.__size ** 2)
+    @size.setter
+    def size(self, value):
+        """ setter function for private attribute size
+            Args:
+                value (int): value to be set
+            Return:
+                nothing
+        """
+        if not (isinstance(value, int)):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
 
-
-@property
-def size(self):
-    """To retrieve size"""
-    return self.__size
-
-
-@size.setter
-def size(self, value):
-    """To set size"""
-    if not isinstance(value, int):
-        raise TypeError("size must be an integer")
-    if value < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = value
+    def area(self):
+        """
+             area of the square.
+        """
+        return self.__size * self.__size
