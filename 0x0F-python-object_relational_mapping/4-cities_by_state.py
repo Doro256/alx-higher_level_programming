6f15d0f79a37e31818ab4548bf3ef179a7545b53
Doro256 <dorothynantumbwe44@gmail.com>
2022-10-09 19:12:12 +0000

    Cities by state

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-A script that takes in an argument and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument.
+A script that lists all states from the database hbtn_0e_0_usa.
 """
 import MySQLdb
 import sys
@@ -11,14 +10,10 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT \
-                 cities.id AS ID, \
-                 cities.name AS city\
-                 states.name AS state
-                 FROM cities \
-                 LEFT JOIN states ON cities.state_id = states.id\
-		 ORDER BY cities.id ASC")
-
+    cur.execute("SELECT cities.id, cities.name, states.name\
+                 FROM cities\
+                 LEFT JOIN states ON cities.state_id=states.id\
+                 ORDER BY cities.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
