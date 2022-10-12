1984c31894d8cf92c90abe68ad6f0da5670a8191
Doro256 <dorothynantumbwe44@gmail.com>
2022-10-12 19:25:23 +0000

    States with a

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This script lists all State objects from the database hbtn_0e_6_usa
+This script lists all State objects that contain the letter
+a from the database hbtn_0e_6_usa
 """
 
 
